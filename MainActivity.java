ef0ac450b81e86aeb831d7992937830c4cfafd20
harshit1200 <105700811+harshit1200@users.noreply.github.com>
2022-05-30 21:38:17 +0530

    code of problem 2 added

--- MainActivity.java
+++ MainActivity.java
@@ -1,27 +1,37 @@
-package com.example.sonali_app3;
+package com.example.sonali_app4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-    boolean isegg=true;
+    boolean isdog=true;
     public void change(View view){
-        ImageView iv = findViewById(R.id.imageView);
-        if(isegg){
-            iv.setImageResource(R.drawable.cat);
-            isegg=false;
+
+        ImageView imageView = findViewById(R.id.imageView);
+        ImageView imageView2 = findViewById(R.id.imageView4);
+        if(isdog){
+            imageView.animate().setDuration(2000).alpha(1);
+            imageView2.animate().setDuration(2000).alpha(0);
+            isdog=false;
         }else{
-            iv.setImageResource(R.drawable.egg2);
-            isegg=true;
+            imageView.animate().setDuration(2000).alpha(0);
+            imageView2.animate().setDuration(2000).alpha(1);
+            isdog=true;
+
         }
+
+
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView imageView = findViewById(R.id.imageView);
+        imageView.animate().setDuration(2000).alpha(0);
     }
 }
